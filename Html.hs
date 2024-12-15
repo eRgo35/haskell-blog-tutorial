12307516145aa798fc4b3dff15625e0db0d2bb50
Michał Czyż <mike@c2yz.com>
2024-12-15 14:19:43 +0100

    feat: parser lib finished

--- Html.hs
+++ Html.hs
@@ -1,66 +1,16 @@
 module Html
-    ( Html
-    , Title
-    , Structure
-    , html_
-    , p_
-    , h1_
-    , append_
-    , render
-    )
-    where
+  ( Html,
+    Title,
+    Structure,
+    html_,
+    h1_,
+    p_,
+    ul_,
+    ol_,
+    code_,
+    append_,
+    render,
+  )
+where
 
-newtype Html 
-    = Html String
-newtype Structure 
-    = Structure String
-
-type Title
-    = String
-
-html_ :: Title -> Structure -> Html
-html_ title content =
-    Html
-        ( el "html"
-            ( el "head" (el "title" title) 
-              <> el "body" (getStructureString content)
-            )
-        )
-
-p_ :: String -> Structure
-p_ = Structure . el "p"
-
-h1_ :: String -> Structure
-h1_ = Structure . el "h1"
-
-el :: String -> String -> String
-el tag content = 
-    "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
-
-append_ :: Structure -> Structure -> Structure
-append_ c1 c2 =
-    Structure (getStructureString c1 <> getStructureString c2)
-
-getStructureString :: Structure -> String
-getStructureString content =
-    case content of
-        Structure str -> str
-
-render :: Html -> String
-render html =
-    case html of
-        Html str -> str
-
-escape :: String -> String
-escape = 
-    let
-        escapeChar c =
-            case c of
-                '<' -> "&lt;"
-                '>' -> "&gt;"
-                '&' -> "&amp;"
-                '"' -> "&quot;"
-                '\'' -> "&&#39;"
-                _ -> [c]
-    in
-        concat . map escapeChar
+import Html.Internal
--- parser.hs
+++ parser.hs
@@ -4,13 +4,13 @@ main :: IO ()
 main = putStrLn (render myhtml)
 
 myhtml :: Html
-myhtml = 
-    html_
-        "My Page"
-        ( append_ 
-            (h1_ "Hello, World!")
-            (append_ 
-                (p_ "This is a paragraph.")
-                (p_ "This is another paragraph.")
-            )
+myhtml =
+  html_
+    "My Page"
+    ( append_
+        (h1_ "Hello, World!")
+        ( append_
+            (p_ "This is a paragraph.")
+            (p_ "This is another paragraph.")
         )
+    )
